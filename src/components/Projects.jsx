ebe6f5c3fc8f19e43f4a6e02ba44b1fb56c38fed
Abhishesk Singh <abhisheksingh80024@gmail.com>
2026-06-07 16:31:07 +0530

    changePortfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -36,7 +36,7 @@ export default function Projects({ darkMode }) {
       ],
       tech: ['React.js', 'HTML5', 'Tailwind CSS3','Jquery'],
       deployUrl: 'https://6a1b258149268e7fecb79dba--super-otter-32abd1.netlify.app/',
-      githubUrl: 'https://github.com/Abhishek00024?tab=repositories',
+      githubUrl: 'https://github.com/Abhishek00024/Doctors_Appointment_Booking',
       deployUrl: 'https://6a1b258149268e7fecb79dba--super-otter-32abd1.netlify.app/',
       icon: <Database className="w-6 h-6 text-neon-magenta" />,
       accentColor: 'neon-magenta',
